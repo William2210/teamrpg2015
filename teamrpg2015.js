69c7d0c1571183e1378ee84041e4ba8b28fc1769
William2210 <34014293+William2210@users.noreply.github.com>
2018-01-08 19:50:25 +1100

    Test Editor using Visual Studio
    Changes made using visual studio program as test process . Included JavaScript files as a means of responsive sidebar panel learning.

--- teamrpg2015.js
+++ teamrpg2015.js
@@ -1,10 +1,10 @@
-function sidebar_open() {
+function w3_open() {
     document.getElementById("main").style.marginLeft = "25%";
     document.getElementById("mySidebar").style.width = "25%";
     document.getElementById("mySidebar").style.display = "block";
     document.getElemebtById("openNav").style.display ='none';
 }
-function sidebar_close() {
+function w3_close() {
     document.getElementById("main").style.marginLeft = "0%";
     document.getElementById("mySidebar").style.display = "none";
     document.getElementById("openNav").style.display = "inline-block";
