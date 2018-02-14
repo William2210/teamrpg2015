26b37a6635be4139a659e7c9e864bba8a838f3a6
William2210 <34014293+William2210@users.noreply.github.com>
2018-02-14 16:50:39 +1100

    New Sidebar and content update using Bootstrapping example

--- teamrpg2015.js
+++ teamrpg2015.js
@@ -1,11 +1,6 @@
-function w3_open() {
-    document.getElementById("main").style.marginLeft = "25%";
-    document.getElementById("mySidebar").style.width = "25%";
-    document.getElementById("mySidebar").style.display = "block";
-    document.getElemebtById("openNav").style.display ='none';
-}
-function w3_close() {
-    document.getElementById("main").style.marginLeft = "0%";
-    document.getElementById("mySidebar").style.display = "none";
-    document.getElementById("openNav").style.display = "inline-block";
-}
+$(document).ready(function () {
+    
+    $('#sidebarCollapse').on('click', function () {
+        $('#sidebar').toggleClass('active');
+    });
+});
